c784d9cae5bbe9892cff105061ff13868dd878e4
Aniluffi <90216018+Aniluffi@users.noreply.github.com>
2023-12-06 09:51:53 +0200

    Add files via upload

---
+++ зд4/ConsoleApp1/ConsoleApp1/Figura.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Figura
+    {
+        string kindFigur;
+        int height;
+        int width;
+
+        public Figura()
+        {
+
+        }
+
+        public Figura(string kindFigur, int height, int width)
+        {
+            this.kindFigur = kindFigur;
+            this.height = height;
+            this.width = width;
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"Тип фигуры - {kindFigur}\n" +
+                $"Высота - {height} см\n" +
+                $"Ширина - {width} см\n" +
+                $"Площадь - {height * width} см кв\n");
+        }
+    }
+}
---
+++ зд4/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApp1;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp67
+
+{
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            Figura fig1 = new Figura("прямоугольник",20,25);
+            fig1.Info();
+        }
+    }
+
+
+
+}
